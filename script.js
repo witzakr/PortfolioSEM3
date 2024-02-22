d4ab44c5a04d5bb76dfe3854fd7e39e93bd61cbe
witzakr <wit.zakrzewski1337@gmail.com>
2024-02-22 23:03:46 +0100

    start

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('weekSelect').addEventListener('change', function() {
+    var selectedOption = this.value;
+    var allWeeks = document.querySelectorAll('.weeks li');
+    allWeeks.forEach(function(week) {
+        week.classList.add('hidden');
+    });
+    document.querySelectorAll('.' + selectedOption).forEach(function(week) {
+        week.classList.remove('hidden');
+    });
+});
