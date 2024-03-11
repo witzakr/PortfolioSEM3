645b21746bdb072ff7d3f97c4db9bb05494d591f
witzakr <wit.zakrzewski1337@gmail.com>
2024-03-11 15:09:18 +0100

    getting rid of the week1 page, reducing the number of html files

--- script.js
+++ script.js
@@ -19,4 +19,19 @@ document.getElementById('weekSelect').addEventListener('change', function() {
         weekText = "Week<span class='red-text'> 16-18</span>";
     }
     document.querySelector('.mainpage h2').innerHTML = weekText;
+
+
+    
 });
+
+var currentImageIndex = 0;
+var images = ["../images/me.jpg", "../images/mesasf.jpg", "../images/mesasf.jpg"];
+
+function changeImage(direction) {
+    if (direction === 'next') {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+    } else if (direction === 'previous') {
+        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    }
+    document.getElementById('profile-image').src = images[currentImageIndex];
+}
