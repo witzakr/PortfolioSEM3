b9ab3955345520630cbdd5c0a09b040219ceec6e
witzakr <wit.zakrzewski1337@gmail.com>
2024-03-12 20:23:44 +0100

    improvements

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-document.getElementById('weekSelect').addEventListener('change', function() {
-    var selectedOption = this.value;
-    var allWeeks = document.querySelectorAll('.weeks li');
-    allWeeks.forEach(function(week) {
-        week.classList.add('hidden');
-    });
-    document.querySelectorAll('.' + selectedOption).forEach(function(week) {
-        week.classList.remove('hidden');
-    });
-    
-    var weekText = "";
-    if (selectedOption === "week1to5") {
-        weekText = "Week<span class='red-text'> 1-5</span>";
-    } else if (selectedOption === "week6to10") {
-        weekText = "Week<span class='red-text'> 6-10</span>";
-    } else if (selectedOption === "week11to15") {
-        weekText = "Week<span class='red-text'> 11-15</span>";
-    } else if (selectedOption === "week16to18") {
-        weekText = "Week<span class='red-text'> 16-18</span>";
-    }
-    document.querySelector('.mainpage h2').innerHTML = weekText;
-
-
-    
-});
 
 var currentImageIndex = 0;
 var images = ["../images/me.jpg", "../images/mesasf.jpg", "../images/mesasf.jpg"];
