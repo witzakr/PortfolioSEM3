0a9cbb4eeb30a1910c45d868bc41366d9e3b2b38
witzakr <wit.zakrzewski1337@gmail.com>
2024-03-15 19:40:14 +0100

    slideshow and first assigmnets

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function changeImage(direction) {
     }
     document.getElementById('profile-image').src = images[currentImageIndex];
 }
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+var i;
+var x = document.getElementsByClassName("mySlides");
+if (n > x.length) {slideIndex = 1}
+if (n < 1) {slideIndex = x.length} ;
+for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+}
+x[slideIndex-1].style.display = "block";
+}
